34d49beb610d7316b144b318bd22fe71a550e253
Pat Hermens <p.hermens@coolblue.nl>
2017-03-02 20:20:44 +0100

    Adding the guts

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Amazon.Lambda.Core;
+
+namespace MyFirstLambdaFunction
+{
+    public class HelloWorld
+    {
+        public static void SayHi(string name = "World", ILambdaContext context = null)
+        {
+            context.Logger.LogLine($"Hello {(String.IsNullOrEmpty(name) ? "World" : name)}!");
+            context.Logger.LogLine($"The time is {DateTime.UtcNow.ToString("U")}Z.");
+            context.Logger.LogLine($"Function name: {context?.FunctionName ?? "Unavailable"}.");
+            context.Logger.LogLine($"RemainingTime: {context?.RemainingTime.ToString() ?? "Unavailable"}.");
+        }
+        public static void Main(string[] args)
+        {
+            SayHi(args.FirstOrDefault());
+        }
+    }
+}
